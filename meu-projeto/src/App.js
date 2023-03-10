c9b0c3ddde13483b6d10e22cd71cc618e0fc508c
Pedro <peh.gois@gmail.com>
2023-03-10 13:43:37 -0300

    Criação das rotas

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,12 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Home from './components/pages/Home';
+import Contact from './components/pages/Contact';
+import Company from './components/pages/Company';
+import NewProject from './components/pages/NewProject';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Olá mundo</h1>
-      <p>Meu primeiro app</p>
-    </div>
+    <Router>
+
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contato</Link>
+        <Link to="/company">Empresa</Link>
+        <Link to="/newproject">Novo projeto</Link>
+      </div>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/company" element={<Company />} />
+        <Route path="/newproject" element={<NewProject />} />
+      </Routes>
+ 
+    </Router>
   );
 }
 
